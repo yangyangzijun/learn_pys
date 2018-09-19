88ec454601276c20f1fc26013244cf21501d3f54
yangyangzijun <811295558@qq.com>
2018-09-19 18:57:18 +0800

    Initial commit

---
+++ third/headers_1.py
@@ -0,0 +1,13 @@
+import  urllib.request
+url="http://www.baidu.com"
+headers=("User-Agent","Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36")
+
+
+file=urllib.request.build_opener()
+file.addheaders=[headers]
+data=file.open(url).read()
+
+local=open("F:\py/3.html","wb")
+local.write(data)
+local.close()
+
