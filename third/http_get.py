ef66f99fc589e255c705566b58f6ed48dd5f6608
yangyangzijun <811295558@qq.com>
2018-09-19 20:06:38 +0800

    pp

--- third/http_get.py
+++ third/http_get.py
@@ -1,6 +1,7 @@
 import urllib.request
-keyword="hello"
-url="http://www.baidu.com/s?wd="+keyword
+keyword="王"
+key_word=urllib.request.quote(keyword)
+url="http://www.baidu.com/s?wd="+key_word
 req=urllib.request.Request(url)
 file=urllib.request.urlopen(req)
 local=open("F:\py/6.html","wb")
